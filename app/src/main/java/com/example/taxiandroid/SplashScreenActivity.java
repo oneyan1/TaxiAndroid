a367d9fc4320b9d4af4682f5977b81e226e30e96
oneyan1 <yangrodno@gmail.com>
2020-01-29 00:37:30 +0100

    splash screen added

---
+++ app/src/main/java/com/example/taxiandroid/SplashScreenActivity.java
@@ -0,0 +1,35 @@
+package com.example.taxiandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try{
+                    sleep(5000);
+                }catch(Exception e){
+                    e.printStackTrace();
+                }finally {
+                    startActivity(new Intent(SplashScreenActivity.this, SignInActivity.class));
+                }
+            }
+        };
+
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
